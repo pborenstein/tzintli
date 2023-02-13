fe6eaa6ad18830732addaebee8bb0282dfcfb58b
Philip Borenstein <pborenstein@gmail.com>
2023-02-12 20:13:59 -0500

    stupid breaking changes

--- js/oembed.js
+++ js/oembed.js
@@ -17,17 +17,17 @@ module.exports = async function oembed(url, params) {
     return oembed_data.html
   }
 
-  switch ( provider.providerName ) {
+  switch ( provider.endpoint ) {
         case 'The New York Times':
           return microlink(url)
           return nytHandler(url, params)
             break
 
-        case 'Twitter':
+        case 'https://publish.twitter.com/oembed/':
           return twitterHandler(url, params)
             break
 
-        case 'Flickr':
+        case "https://www.flickr.com/services/oembed/":
           return flickrHandler(url, params)
             break
 
